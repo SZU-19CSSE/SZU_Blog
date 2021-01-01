1381c6977ba3bb71baa22054017e5a43c6208033
沈晨玙 <939826879@qq.com>
2021-01-01 12:44:38 +0800

    新增用户
    视频21 路由设置及传参

--- model/user.js
+++ model/user.js
@@ -1,56 +1,56 @@
 //引入mongoose第三方模块
-const mongoose =require('mongoose');
+const mongoose = require('mongoose');
 //导入bcrypt
-const bcrypt =require('bcrypt');
+const bcrypt = require('bcrypt');
 //创建用户集合规则
-const userSchema=new mongoose.Schema({
-    username :{
-        type:String,
-        require:true,
-        minlength:2,
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        require: true,
+        minlength: 2,
         maxlength: 20
     },
-    email:{
-        type:String,
+    email: {
+        type: String,
         //保证邮箱不重复
         unique: true,
-        required:true
+        required: true
     },
-    password:{
-        type:String,
-        require:true
+    password: {
+        type: String,
+        require: true
     },
     //admin:超级管理员
     //normal:普通用户
-    role:{
-        type:String,
-        require:true
+    role: {
+        type: String,
+        require: true
     },
-        // 0 启用状态
-        // 1 禁用状态
-    state:{
-        type:Number,
-        default:0
+    // 0 启用状态
+    // 1 禁用状态
+    state: {
+        type: Number,
+        default: 0
     }
 
 
 });
 //创建集合
-const User=mongoose.model('User',userSchema);
+const User = mongoose.model('User', userSchema);
 
-async function createUser(){
-    const salt=await bcrypt.genSalt(10);
-    const pass=await bcrypt.hash('123456',salt);
-    const user=await User.create({
-        username:'itheima',
-        email:'itheima@itcast.cn',
-        password:pass,
-        role:'admin',
-        state:0
+async function createUser() {
+    const salt = await bcrypt.genSalt(10);
+    const pass = await bcrypt.hash('123456', salt);
+    const user = await User.create({
+        username: 'itheima',
+        email: 'itheima@itcast.cn',
+        password: pass,
+        role: 'admin',
+        state: 0
     });
 }
 // createUser();
 //  将用户集合做为模块成员进行导出
-module.exports={ 
+module.exports = {
     User
 };
--- route/admin.js
+++ route/admin.js
@@ -4,14 +4,20 @@ const express = require('express');
 
 //创建博客展示页面路由
 const admin = express.Router()
-//实现登陆功能
-admin.post('/login',require('./admin/login') );
+    //实现登陆功能
+admin.post('/login', require('./admin/login'));
 //渲染登录页面
-admin.get('/login',require('./admin/loginPage'));
+admin.get('/login', require('./admin/loginPage'));
 //退出功能
-admin.get('/logout',require("./admin/logout"));
+admin.get('/logout', require("./admin/logout"));
 //创建用户列表路由
 admin.get('/user', require('./admin/userPage'));
+//创建用户编辑页面路由
+admin.get('/user-edit', require('./admin/user-edit'))
+    //创建实现用户添加功能路由
+admin.post('/user-edit', require('./admin/user-edit-fn'))
+
+
 admin.get('/article', (req, res) => {
     res.render('admin/article')
 });

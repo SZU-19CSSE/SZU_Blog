530ada92b5b230894cdd19f0a92ea2254df631aa
沈晨玙 <939826879@qq.com>
2021-01-01 17:36:31 +0800

    分页功能
    视频29 页面跳转按钮

--- route/admin/userPage.js
+++ route/admin/userPage.js
@@ -5,7 +5,7 @@ module.exports = async(req, res) => {
     //接收客户端传递过来的当前页参数
     let page = req.query.page || 1;
     //每一页显示的数据条数
-    let pagesize = 10;
+    let pagesize = 3;
     //查询用户数据的总数
     let count = await User.countDocuments({});
     //总页数
@@ -19,6 +19,8 @@ module.exports = async(req, res) => {
     //渲染用户列表模块
     res.render('admin/user', {
         users: users,
+        page: page,
+        total: total,
         msg: req.session.username
     });
 }

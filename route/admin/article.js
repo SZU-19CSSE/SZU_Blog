503eb862f4534e0521936c46b712d1bb9b38c7fc
CR7-source <2019285006@email.szu.edu.cn>
2021-01-02 14:30:52 +0800

    07-08
    08 多表查询 populate

--- route/admin/article.js
+++ route/admin/article.js
@@ -1,5 +1,13 @@
-module.exports=(req, res) => {
+//将文章集合的构造函数导入到当前文件中
+const {Article}=require('../../model/article');
+module.exports= async(req, res) => {
         //标识 标识当前访问的是文章管理页面
         req.app.locals.currentLink='article';
-    res.render('admin/article')
+        //查询所有文章数据
+        let articles=await Article.find().populate('author').lean();
+        // res.send(articles);
+        //渲染文章模板
+        res.render('admin/article.art',{
+            articles:articles
+        });
 }

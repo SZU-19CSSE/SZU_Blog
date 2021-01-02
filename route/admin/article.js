0b33a70621be65d7b1f5beaed1280519c45df1ab
沈晨玙 <939826879@qq.com>
2021-01-02 18:25:32 +0800

    1

--- route/admin/article.js
+++ route/admin/article.js
@@ -5,7 +5,7 @@ const pagination = require('mongoose-sex-page');
 
 module.exports = async(req, res) => {
     // 接收客户端传递过来的页码
-    const page = req.query.page;
+    // const page = req.query.page;
     // 标识 标识当前访问的是文章管理页面
     req.app.locals.currentLink = 'article';
     // page 指定当前页
@@ -13,12 +13,12 @@ module.exports = async(req, res) => {
     // display 指定客户端要显示的页码数量
     // exec 向数据库中发送查询请求
     // 查询所有文章数据
-    let articles = await pagination(Article).find().page(page).size(2).display(3).populate('author').exec();
+    let articles = await pagination(Article).find().page(1).size(2).display(3).populate('author').exec();
 
-    res.send(articles);
+    // res.send(articles);
 
     // 渲染文章列表页面模板
-    // res.render('admin/article.art', {
-    //     articles: articles
-    // });
+    res.render('admin/article.art', {
+        articles: articles
+    });
 }

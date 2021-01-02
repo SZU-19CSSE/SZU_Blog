574cfe0f5689d8eb47d2ce04773020e7d28dc063
沈晨玙 <939826879@qq.com>
2021-01-02 19:43:23 +0800

    恢复
    删除分页功能

--- route/admin/article.js
+++ route/admin/article.js
@@ -1,31 +1,13 @@
-<<<<<<< HEAD
-// 将文章集合的构造函数导入到当前文件中
-const { Article } = require('../../model/article');
-// 导入mongoose-sex-page模块
-const pagination = require('mongoose-sex-page');
-
-module.exports = async(req, res) => {
-    // 接收客户端传递过来的页码
-    // const page = req.query.page;
-    // 标识 标识当前访问的是文章管理页面
-    req.app.locals.currentLink = 'article';
-    // page 指定当前页
-    // suze 指定每页显示的数据条数
-    // display 指定客户端要显示的页码数量
-    // exec 向数据库中发送查询请求
-    // 查询所有文章数据
-    let articles = await pagination(Article).find().page(1).size(2).display(3).populate('author').exec();
-
-    // res.send(articles);
-
-    // 渲染文章列表页面模板
-    res.render('admin/article.art', {
-        articles: articles
-    });
-=======
-module.exports=(req, res) => {
+//将文章集合的构造函数导入到当前文件中
+const {Article}=require('../../model/article');
+module.exports= async(req, res) => {
         //标识 标识当前访问的是文章管理页面
         req.app.locals.currentLink='article';
-    res.render('admin/article')
->>>>>>> parent of 503eb86... 07-08
+        //查询所有文章数据
+        let articles=await Article.find().populate('author').lean();
+        // res.send(articles);
+        //渲染文章模板
+        res.render('admin/article.art',{
+            articles:articles
+        });
 }
